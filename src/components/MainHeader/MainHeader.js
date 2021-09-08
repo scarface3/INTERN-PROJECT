576006407e0a834c50735c8dec4a4ea20a38e3eb
scarface3 <49342290+scarface3@users.noreply.github.com>
2021-09-08 20:16:13 +0100

    Create MainHeader.js

---
+++ src/components/MainHeader/MainHeader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Navigation from "./Navigation";
+import classes from "./MainHeader.module.css";
+
+const MainHeader = (props) => {
+  return (
+    <header className={classes["main-header"]}>
+      <Navigation />
+    </header>
+  );
+};
+
+export default MainHeader;
